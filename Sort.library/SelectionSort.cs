606dfcd529d96747b7f88ab64860006514791eb2
svb <svb@example.com>
2025-02-12 08:40:44 +0000

    selection sort uitbreiding

--- Sort.library/SelectionSort.cs
+++ Sort.library/SelectionSort.cs
@@ -4,7 +4,14 @@ namespace Sort.library
 {
     public class SelectionSort
     {
+        // Selection sort voor integers
         public static int[] Sort(int[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Selection sort voor strings
+        public static int[] Sort(string[] input)
         {
             throw new NotImplementedException();
         }
