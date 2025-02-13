c7ef3d727d7211c9fd16ceed2378261c626d4c9e
Mariën Sven <p100415@ap.be>
2025-02-13 10:55:07 +0100

    random generator added comment, removed exception

--- Sort.library/RandomArrayGenerator.cs
+++ Sort.library/RandomArrayGenerator.cs
@@ -4,8 +4,18 @@ namespace Sort.library
 {
     public class RandomArrayGenerator
     {
+        /// <summary>
+        /// Generate a list of random numbers 
+        /// </summary>
+        /// <param name="amount">The size of the list</param>
+        /// <param name="min">The minimum value</param>
+        /// <param name="max">The maximum value</param>
+        /// <param name="unique">No 2 numbers can be the same</param>
+        /// <returns>The list with random numbers</returns>
         public static int[] Generate(int amount = 10, int min = 0, int max = 10, bool unique = false) {
-            throw new NotImplementedException();
+            //TODO: implement random generator and return the
+            //list of random numbers
+            return null;
         }
     }
 }
